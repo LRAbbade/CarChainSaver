4de49371b30cea92c1f3338e618552a4c32b16eb
Lucas Abbade <lucasabbade@hotmail.com>
2018-09-19 21:45:23 -0300

    testing

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from endpoint import endpoint
 from datetime import datetime
 import json
 import requests
+import time
 
 client = get_mongo_client()
 db = client.carChain
@@ -25,22 +26,38 @@ for document in cursor:
     total_meters = [(round(Coordinate.Distance(trip[i-1][0], trip[i][0])), trip[i][1]) for i in range(1, len(trip))]
     to_save[plate].extend(total_meters)
 
+correct_plates = {
+    '1': 'TST-0001',
+    '2': 'TST-0002',
+    '3': 'TST-0003',
+    '4': 'TST-0004',
+    '5': 'TST-0005'
+}
+
 jsons = []
 
 for plate, trips in to_save.items():
     for meters, timestamp in trips:
         jsons.append({
             'timestamp': timestamp,
-            'carPlate': plate,
+            'carPlate': correct_plates[plate],
             'block': {
                 'data': meters
             }
         })
 
-with FuturesSession(max_workers=1) as session:
-    futures = []
-    for obj in jsons:
-        futures.append(session.post(endpoint, json=obj))      # TODO: this should be updated when a final request type is defined
+# TODO: juntar total por dia pra mandar
 
-    for future in futures:
-        pprint(future.result())     # result from the request, should have information about the acceptance of the block
+for obj in jsons:
+    r = requests.post(endpoint, json=obj)
+    print(r.status_code)
+    pprint(json.loads(r.content))
+    time.sleep(2)
+
+# with FuturesSession(max_workers=20) as session:
+#     futures = []
+#     for obj in jsons:
+#         futures.append(session.post(endpoint, json=obj))      # TODO: this should be updated when a final request type is defined
+
+#     for future in futures:
+#         pprint(future.result())     # result from the request, should have information about the acceptance of the block
